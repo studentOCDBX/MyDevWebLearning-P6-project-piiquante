cfc96dc3570fa2ce4230bfd72c783cf65a26dad1
Nazaire DARBOUX <studentdbx@gmail.com>
2023-04-02 05:58:29 +0200

    installation of CORS, mongoose-unique-validator,bcrypt, jsonwebtoken and implémentation of user schema and his signup and login functions

---
+++ backend/controllers/user.js
@@ -0,0 +1,47 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((Error) => res.status(400).json({ Error }));
+    })
+    .catch((Error) => res.status(500).json({ Error }));
+};
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ Message: "Paire identifiant/mot de passe incorrect !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              Message: "Paire identifiant/mot de passe incorrect !",
+            });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SCRET", {
+              expiresIn: "24h",
+            }),
+          });
+        })
+        .catch((Error) => res.status(500).json({ Error }));
+    })
+    .catch((Error) => res.status(500).json({ Error }));
+};
